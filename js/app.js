174d754b6883e650b23db10a2c42143f8639cb51
RTBevilacqua <rtb983@yahoo.com>
2016-09-09 15:35:58 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,86 +1,83 @@
 $(document).ready(function(){
 	var number = numberGen();
-	
+	var count = 0;
+
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+ $(".what").click(function(){
+   $(".overlay").fadeIn(1000);
+ });
 
-  	});
-
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
-
-
-
-/*------- New Game ------------*/
-$('.new').click(function () {
-document.location.reload(true);
+ /*--- Hide information modal box ---*/
+ $("a.close").click(function(){
+  $(".overlay").fadeOut(1000);
 });
 
-/*----------- User Input ----------------*/  
-$('form').submit(function(e){
-	e.preventDefault(); 
-	// console.log(hat);
-
+ /*------- New Game ------------*/
+ $('.new').click(function () {
+   $('#feedback').text('Make your Guess!');
+     //makes counter and input start over
+   $('#guessList').empty();
+   count = 0;
+   $('#count').text(count);
+   $('#userGuess').val("");
+   number = numberGen();
+   //button becomes enabled.
+   $('#guessButton').prop('disabled', false);  
 });
+
+ /*----------- User Input ----------------*/  
+ $('form').submit(function(e){
+   e.preventDefault(); 
+   $('#feedback').empty();
+   var guessVal = $('#userGuess').val();
+   $('#guessList').append('<li>' + guessVal + '</li>');
+   guessVal = Number(guessVal);
+   formula(guessVal);
+ });
 
 // /*----------- Number Checker ----------------*/
 var formula = function(hat){
-	console.log(typeof(hat));
-  	if(hat === number) {
-  		$('#feedback').text('YOU FIGURED IT OUT! YOU\'RE SO SMART!');
-  	}
-  	else if (hat > number - 3 && hat < number + 3){
-  		$('#feedback').text('You\'re getting warmer!');
-  	}
+  count++;
+  $('#count').html(count);
   
-  	else if (hat > number - 15 && hat < number + 15){
-  		$('#feedback').text('You\'re getting warmer!');
-
-  	}
-  	else if (isNaN(hat)) {
-  		$('#feedback').text('Please enter a number.');
-  	}
-  	else {
-  		$('#feedback').text('Cold cold cold!');
-  	};
-  };
-
-/*-------------- Hot or Cold Saying --------------*/
-$('form').on('click', '#guessButton', function(){
-	$('h2#feedback').empty();
-	var hat = $('#userGuess').val();
-	$('ul').append('<li>' + hat + '</li>');
-	hat = parseInt(hat);
-	formula(hat);
-  });
-	
-
-/*-------------- Saved Number of Guesses----------*/
-$('#guessButton').click(function(){
-	update($('#count'));
-
-});
-
-function update(j) {
-	var n = parseInt(j.text(), 10);
-	j.text( n + 1);
-
+//When figured out the button gets disabled.
+ if(hat === number) {
+  $('#feedback').text('YOU FIGURED IT OUT! YOU\'RE SO SMART!');
+  $('#guessButton').prop('disabled', true);
+  return;
+}
+else if (hat > number - 3 && hat < number + 3){
+  $('#feedback').text('You\'re getting warmer!');
+  return;
 }
 
+else if (hat > number - 15 && hat < number + 15){
+  $('#feedback').text('You\'re getting warmer!');
+  return;
+}
+else if (isNaN(hat)) {
+  $('#feedback').text('Please enter a number.');
+  return;
+}
+else {
+  $('#feedback').text('Cold cold cold!');
+  return;
+};
+};
+
+
+
+
+/*------- Random Number Generator ------------*/
+function numberGen() {
+  var number = Math.floor((Math.random() * 100) + 1);
+  return number;
+}
 
 /*---------------- End --------------------*/
 });
 
 
-/*------- Random Number Generator ------------*/
-  	function numberGen() {
-  	var number = Math.floor((Math.random() * 100) + 1);
-  	console.log(number);
-  	return number;
-  	};
 
 
 
